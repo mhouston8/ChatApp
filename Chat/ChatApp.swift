865fad71512b50e5b1f74978aa7a92da1fb51350
Matthew Houston <matthew.houston7@icloud.com>
2023-03-05 11:46:23 -0800

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Matthew Houston on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
